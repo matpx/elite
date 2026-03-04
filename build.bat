0b888d45d5da6447ea537bac6a41330ef0d29c02
matpx <9219741+matpx@users.noreply.github.com>
2026-03-04 23:34:14 +0100

    tcc windows binaries

--- build.bat
+++ build.bat
@@ -1,13 +1,10 @@
 @echo off
 
-rem download this:
-rem https://nuwen.net/mingw.html
-
 echo compiling (windows)...
 
 windres res.rc -O coff -o res.res
-gcc src/*.c src/api/*.c src/lib/lua52/*.c src/lib/stb/*.c^
-    -O3 -s -std=gnu11 -fno-strict-aliasing -Isrc -DLUA_USE_POPEN^
+winlib\tcc\tcc.exe -Bwinlib\tcc src/*.c src/api/*.c src/lib/lua52/*.c src/lib/stb/*.c^
+    -O3 -s -std=c11 -fno-strict-aliasing -Isrc -DLUA_USE_POPEN^
     -Iwinlib/SDL3-3.4.2/x86_64-w64-mingw32/include^
     -lmingw32 -lm -lSDL3 -Lwinlib/SDL3-3.4.2/x86_64-w64-mingw32/lib^
     -mwindows res.res^
--- build.sh
+++ build.sh
@@ -2,43 +2,18 @@
 
 set -e
 
-cflags="-Wall -O3 -g -std=c11 -fno-strict-aliasing -Isrc -D_XOPEN_SOURCE=500"
-lflags="-lSDL3 -lm"
+compiler="${CC:-tcc}"
+cflags="-Wall -O3 -g -std=c11 -fno-strict-aliasing -Isrc -DLUA_USE_POSIX -D_XOPEN_SOURCE=500"
+lflags="-lSDL3 -lm -o lite"
 
-if [[ $* == *windows* ]]; then
-  platform="windows"
-  outfile="lite.exe"
-  compiler="x86_64-w64-mingw32-gcc"
-  cflags="$cflags -DLUA_USE_POPEN -Iwinlib/SDL3-3.4.2/x86_64-w64-mingw32/include"
-  lflags="$lflags -Lwinlib/SDL3-3.4.2/x86_64-w64-mingw32/lib"
-  lflags="-lmingw32 $lflags -mwindows -o $outfile res.res"
-  x86_64-w64-mingw32-windres res.rc -O coff -o res.res
-else
-  platform="unix"
-  outfile="lite"
-  compiler="gcc"
-  cflags="$cflags -DLUA_USE_POSIX"
-  lflags="$lflags -o $outfile"
-fi
-
-if command -v ccache >/dev/null; then
-  compiler="ccache $compiler"
-fi
-
-echo "compiling ($platform)..."
+echo "compiling..."
 for f in `find src -name "*.c"`; do
   $compiler -c $cflags $f -o "${f//\//_}.o"
-  if [[ $? -ne 0 ]]; then
-    got_error=true
-  fi
 done
 
-if [[ ! $got_error ]]; then
-  echo "linking..."
-  $compiler *.o $lflags
-fi
+echo "linking..."
+$compiler *.o $lflags
 
 echo "cleaning up..."
 rm -f *.o
-rm -f res.res
 echo "done"

68942d8d3410f9e48801007cd0ebf6aacc80b6c6
matpx <9219741+matpx@users.noreply.github.com>
2026-03-04 23:45:17 +0100

    debug flag

--- build.sh
+++ build.sh
@@ -3,8 +3,15 @@
 set -e
 
 compiler="${CC:-tcc}"
-cflags="-Wall -O3 -g -std=c11 -fno-strict-aliasing -Isrc -DLUA_USE_POSIX -D_XOPEN_SOURCE=500"
+cflags="-Wall -O3 -std=c11 -fno-strict-aliasing -Isrc -DLUA_USE_POSIX -D_XOPEN_SOURCE=500"
 lflags="-lSDL3 -lm -o lite"
+
+if [[ $* == *debug* ]]; then
+  cflags="$cflags -g"
+
+  # cflags="$cflags -b"
+  # lflags="$lflags -b"
+fi
 
 echo "compiling..."
 for f in `find src -name "*.c"`; do

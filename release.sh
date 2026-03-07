7382b6df4ad40bc0b66fcee9ec5950f7533ee50c
matpx <9219741+matpx@users.noreply.github.com>
2026-03-07 01:39:31 +0100

    add LTO to release build

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-CC="x86_64-w64-mingw32-gcc" ./build.sh windows
-CC="gcc" ./build.sh
+CC="x86_64-w64-mingw32-gcc -flto=auto" ./build.sh windows
+CC="gcc -flto=auto" ./build.sh
 
 cp winlib/SDL3-3.4.2/x86_64-w64-mingw32/bin/SDL3.dll SDL3.dll
 strip SDL3.dll

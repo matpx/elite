de0514179eb577e18583aada77b13a19e1e4231f
matpx <9219741+matpx@users.noreply.github.com>
2026-03-07 21:10:57 +0100

    quiet zip

--- release.sh
+++ release.sh
@@ -6,9 +6,10 @@ CC="x86_64-w64-mingw32-gcc -flto=auto" ./build.sh windows
 CC="gcc -flto=auto" ./build.sh
 
 cp winlib/SDL3-3.4.2/x86_64-w64-mingw32/bin/SDL3.dll SDL3.dll
+
 strip SDL3.dll
 strip lite.exe
 strip lite
 
 rm -f lite.zip 2>/dev/null
-zip lite.zip lite lite.exe SDL3.dll data -r -T -9
+zip -qrT9 lite.zip lite lite.exe SDL3.dll data

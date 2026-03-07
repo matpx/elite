d8df263336eec412231cfb9f0e387c0bcb03a5b2
matpx <9219741+matpx@users.noreply.github.com>
2026-03-07 01:27:54 +0100

    fix tcc compat for command buffer alignment

--- src/rencache.c
+++ src/rencache.c
@@ -30,7 +30,8 @@ static unsigned cells_buf2[CELLS_X * CELLS_Y];
 static unsigned *cells_prev = cells_buf1;
 static unsigned *cells = cells_buf2;
 static RenRect rect_buf[CELLS_X * CELLS_Y / 2];
-static _Alignas(Command) char command_buf[COMMAND_BUF_SIZE];
+static union { Command _align; char buf[COMMAND_BUF_SIZE]; } command_buf_union;
+#define command_buf (command_buf_union.buf)
 static int command_buf_idx;
 static RenRect screen_rect;
 static bool show_debug;
@@ -80,7 +81,7 @@ static RenRect merge_rects(RenRect a, RenRect b) {
 
 
 static Command* push_command(int type, int size) {
-  size = (size + _Alignof(Command) - 1) & ~(_Alignof(Command) - 1);
+  size = (size + sizeof(void*) - 1) & ~(sizeof(void*) - 1);
   Command *cmd = (Command*) (command_buf + command_buf_idx);
   int n = command_buf_idx + size;
   if (n > COMMAND_BUF_SIZE) {

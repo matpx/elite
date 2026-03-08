f7faa287a53d2967d5881d448cb8ad1505614cd2
matpx <9219741+matpx@users.noreply.github.com>
2026-03-08 15:17:58 +0100

    rename lang font

--- data/core/style.lua
+++ data/core/style.lua
@@ -8,7 +8,7 @@ style.caret_width = common.round(2 * SCALE)
 style.tab_width = common.round(170 * SCALE)
 
 style.font = renderer.font.load(EXEDIR .. "/data/fonts/font.ttf", 14 * SCALE)
-style.language_font = renderer.font.load(EXEDIR .. "/data/fonts/language.ttf", 14 * SCALE)
+style.language_font = renderer.font.load(EXEDIR .. "/data/fonts/devicon.ttf", 14 * SCALE)
 style.big_font = renderer.font.load(EXEDIR .. "/data/fonts/font.ttf", 34 * SCALE)
 style.icon_font = renderer.font.load(EXEDIR .. "/data/fonts/icons.ttf", 14 * SCALE)
 style.code_font = renderer.font.load(EXEDIR .. "/data/fonts/monospace.ttf", 13.5 * SCALE)

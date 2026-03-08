c43941bdf0f1c2636e183e61ba836e504385e517
matpx <9219741+matpx@users.noreply.github.com>
2026-03-08 14:50:44 +0100

    -Wextra -Werror

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cflags="-Wall -O3 -g -std=c11 -fno-strict-aliasing -Isrc -fno-diagnostics-show-caret"
+cflags="-Wall -Wextra -O3 -g -std=c11 -fno-strict-aliasing -Isrc -fno-diagnostics-show-caret"
 
 if echo "$*" | grep -q "windows"; then
   compiler="${CC:-winlib/tcc/tcc.exe}"

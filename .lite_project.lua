9221557d3f456abe1b35d705208167b08db40b6d
matpx <9219741+matpx@users.noreply.github.com>
2026-03-08 02:11:03 +0100

    runner parse cc

--- .lite_project.lua
+++ .lite_project.lua
@@ -4,9 +4,10 @@ local windows = PLATFORM == "Windows"
 local output = windows and "lite.exe" or "lite"
 
 global { runner = {
+  lang = "cc",
   build = function()
     if not os.execute(windows and "build.bat >runner.txt 2>&1" or "./build.sh >runner.txt 2>&1") then
-      core.open_file("runner.txt")
+      -- core.open_file("runner.txt")
       return false
     end
 

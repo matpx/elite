46cabe80fbdcb77a823f745f587e2bac8cbcb3e9
matpx <9219741+matpx@users.noreply.github.com>
2026-03-10 20:28:37 +0100

    reset VERSION

--- src/main.c
+++ src/main.c
@@ -76,7 +76,7 @@ int main(int argc, char **argv) {
     }
     lua_setglobal(L, "ARGS");
 
-    lua_pushstring(L, "1.11");
+    lua_pushstring(L, "1.0");
     lua_setglobal(L, "VERSION");
 
     lua_pushstring(L, SDL_GetPlatform());

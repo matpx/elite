985c42614215c4656d253c5d50e0a94963dd6889
matpx <9219741+matpx@users.noreply.github.com>
2026-03-12 10:40:24 +0100

    unused tcc flags

--- src/lib/stb/stb_image.c
+++ src/lib/stb/stb_image.c
@@ -1,5 +1,2 @@
-#ifdef __TINYC__
-#define STBI_NO_SIMD
-#endif
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
--- src/lib/stb/stb_image_resize2.c
+++ src/lib/stb/stb_image_resize2.c
@@ -1,5 +1,2 @@
-#ifdef __TINYC__
-#define STBIR_NO_SIMD
-#endif
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image_resize2.h"

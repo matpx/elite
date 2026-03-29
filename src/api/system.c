03d4942626c5f06d8948be21350f3274b62dc55f
matpx <9219741+matpx@users.noreply.github.com>
2026-03-29 20:48:56 +0200

    f_list_dir use libc

--- src/api/system.c
+++ src/api/system.c
@@ -232,21 +232,29 @@ static int f_chdir(lua_State *L) {
 static int f_list_dir(lua_State *L) {
     const char *path = luaL_checkstring(L, 1);
 
-    int count;
-    char **entries = SDL_GlobDirectory(path, "*", 0, &count);
-    if (!entries) {
+    DIR *dir = opendir(path);
+    if (!dir) {
         lua_pushnil(L);
-        lua_pushstring(L, SDL_GetError());
+        lua_pushstring(L, strerror(errno));
         return 2;
     }
 
     lua_newtable(L);
-    for (int i = 0; i < count; i++) {
-        lua_pushstring(L, entries[i]);
-        lua_rawseti(L, -2, i + 1);
+    int i = 1;
+    struct dirent *entry;
+    while ((entry = readdir(dir))) {
+        if (strcmp(entry->d_name, ".") == 0) {
+            continue;
+        }
+        if (strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+        lua_pushstring(L, entry->d_name);
+        lua_rawseti(L, -2, i);
+        i++;
     }
 
-    SDL_free(entries);
+    closedir(dir);
     return 1;
 }
 
